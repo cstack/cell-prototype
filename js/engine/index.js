7ec29e07dded8573bd72b19706774eae2c6ab5a8
Connor Stack <sirconnorstack@gmail.com>
2017-03-04 13:07:35 -0800

    Parse opcodes.

--- js/engine/index.js
+++ js/engine/index.js
@@ -1,10 +1,52 @@
+var opCodes = [
+  "ACTIVATE",
+  "DIE",
+  "JUMP",
+  "JUMP_IF_TRUE",
+  "LABEL",
+  "SENSE_CELL",
+  "SLEEP",
+  "SPLIT",
+  "SUPPRESS",
+];
+
+function parse(programString) {
+  var lines = programString.split("\n");
+  var nonEmptyLines = lines.filter(function(line){
+    return line.length > 0;
+  });
+
+  var commands = [];
+  var errors = [];
+  nonEmptyLines.forEach(function(line) {
+    var parts = line.split(" ");
+    var color = parts[0];
+    var opCode = parts[1];
+    var parameters = parts.slice(2,parts.length);
+
+    var command = {};
+
+    if (opCodes.indexOf(opCode) > -1) {
+      command.opCode = opCode;
+    } else {
+      errors.push(`Unknown op code ${opCode}`);
+      return;
+    }
+
+    command.parameters = parameters;
+
+    commands.push(command);
+  });
+
+  return {
+    success: errors.length == 0,
+    program: commands,
+    errors: errors,
+  };
+}
+
 exports.CellEngine = {
-  parse: function(programString) {
-    return {
-      success: true,
-      program: {},
-    };
-  },
+  parse: parse,
   simulate: function(program) {
     return {
       states: [],
--- js/engine/test/parse.js
+++ js/engine/test/parse.js
@@ -1,5 +1,12 @@
-var expect = require("chai").expect;
 var parse = require("../index.js").CellEngine.parse;
+
+var expect = require("chai").expect;
+var fs = require('fs');
+var path = require('path');
+
+function bufferFile(relPath) {
+  return fs.readFileSync(path.join(__dirname, relPath), "utf-8"); // zzzz....
+}
 
 describe("CellEngine.parse  ", function() {
   it("parses an empty file", function() {
@@ -7,8 +14,68 @@ describe("CellEngine.parse  ", function() {
     var result = parse(programText);
 
     expect(result).to.deep.equal({
-      program: {},
       success: true,
+      errors: [],
+      program: [],
+    });
+  });
+
+  it("parses every op code", function() {
+    var programText = bufferFile("fixtures/all_op_codes.cell");
+    var result = parse(programText);
+
+    expect(result).to.deep.equal({
+      success: true,
+      errors: [],
+      program: [
+        {
+          opCode: "ACTIVATE",
+          parameters: ["UP", "R"],
+        },
+        {
+          opCode: "DIE",
+          parameters: [],
+        },
+        {
+          opCode: "JUMP",
+          parameters: ["test"],
+        },
+        {
+          opCode: "JUMP_IF_TRUE",
+          parameters: ["test"],
+        },
+        {
+          opCode: "LABEL",
+          parameters: ["test"],
+        },
+        {
+          opCode: "SENSE_CELL",
+          parameters: ["UP"],
+        },
+        {
+          opCode: "SLEEP",
+          parameters: [],
+        },
+        {
+          opCode: "SPLIT",
+          parameters: ["UP"],
+        },
+        {
+          opCode: "SUPPRESS",
+          parameters: ["UP", "R"],
+        },
+      ],
+    });
+  });
+
+  it("errors on unrecognized op code", function() {
+    var programText = "- UNKNOWN_OP_CODE"
+    var result = parse(programText);
+
+    expect(result).to.deep.equal({
+      success: false,
+      errors: ["Unknown op code UNKNOWN_OP_CODE"],
+      program: [],
     });
   });
 });
