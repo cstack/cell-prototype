8095be8faf2c242f77893eff6e0f0f7bea0ef859
Connor Stack <sirconnorstack@gmail.com>
2017-03-04 17:50:51 -0800

    Initialize grid with grid model.

--- js/client/index.js
+++ js/client/index.js
@@ -1,7 +1,42 @@
 const CellEngine = require('engine').CellEngine;
 const CellUI = require('./ui').CellUI;
 
+function createGridModel() {
+  var numRows = 9;
+  var numCols = 9;
+
+  var rows = [];
+  for (var rowNum = 0; rowNum < numRows; rowNum++) {
+    var row = []
+    for (var rowCol = 0; rowCol < numCols; rowCol++) {
+      row.push(false);
+    }
+    rows.push(row);
+  }
+  rows[4][4] = true;
+
+  var data = {
+    rows: rows,
+  }
+
+  var notifyListener = function(callback) {
+    callback(data);
+  }
+
+  var listeners = [];
+  return {
+    addListener: function(callback) {
+      listeners.push(callback);
+      notifyListener(callback);
+    },
+    notifyListeners: function() {
+      listeners.forEach(notifyListener);
+    }
+  }
+}
+
 (function() {
-  var UIRoot = CellUI.create();
+  var gridModel = createGridModel();
+  var UIRoot = CellUI.create(gridModel);
   document.body.appendChild(UIRoot);
 })();
--- js/client/ui.js
+++ js/client/ui.js
@@ -1,36 +1,78 @@
+function createProgramEditor() {
+  var editorRoot = document.createElement("div");
+  editorRoot.setAttribute("id", "program-editor");
+
+  var textBox = document.createElement("textarea");
+  textBox.setAttribute("id", "program-text-box");
+  textBox.textContent = "- SPLIT UP\n- SPLIT DOWN";
+  editorRoot.appendChild(textBox);
+
+  var runButton = document.createElement("button");
+  runButton.classList.add("big-button");
+  runButton.textContent = "Run";
+  editorRoot.appendChild(runButton);
+
+  return editorRoot
+}
+
 function createSidebar() {
   var sidebarRoot = document.createElement("div");
   sidebarRoot.setAttribute("id", "sidebar");
 
+  var editor = createProgramEditor();
+  sidebarRoot.appendChild(editor);
+
   return sidebarRoot;
 }
 
-function createGrid() {
-  var gridRoot = document.createElement("div");
-  gridRoot.setAttribute("id", "grid");
+function createGrid(gridModel) {
+  var gridRootElement = document.createElement("div");
+  gridRootElement.setAttribute("id", "grid");
 
-  for (var i = 0; i < 9; i++) {
-    var row = document.createElement("div");
-    row.classList.add("grid-row");
+  var numRows = 9;
+  var numCols = 9;
 
-    for (var j = 0; j < 9; j++) {
-      var space = document.createElement("grid-space");
-      space.classList.add("grid-space");
-      row.appendChild(space);
+  var gridSpaceElements = [];
+  for (var i = 0; i < numRows; i++) {
+    var rowElement = document.createElement("div");
+    rowElement.classList.add("grid-row");
+
+    var rowSpaceElements = [];
+    for (var j = 0; j < numCols; j++) {
+      var spaceElement = document.createElement("grid-space");
+      spaceElement.classList.add("grid-space");
+      rowElement.appendChild(spaceElement);
+      rowSpaceElements.push(spaceElement);
     }
 
-    gridRoot.appendChild(row);
+    gridRootElement.appendChild(rowElement);
+    gridSpaceElements.push(rowSpaceElements);
   }
 
-  return gridRoot;
+  gridModel.addListener(function(gridModel) {
+    for (var row = 0; row < numRows; row++) {
+      for (var col = 0; col < numCols; col++) {
+        var spaceElement = gridSpaceElements[row][col];
+        if (gridModel.rows[row][col]) {
+          spaceElement.classList.remove("grid-space-empty");
+          spaceElement.classList.add("grid-space-cell");
+        } else {
+          spaceElement.classList.remove("grid-space-cell");
+          spaceElement.classList.add("grid-space-empty");
+        }
+      }
+    }
+  });
+
+  return gridRootElement;
 }
 
 var CellUI = {
-  create: function() {
+  create: function(gridModel) {
     var UIRoot = document.createElement("div");
     UIRoot.setAttribute("id", "cell-root");
 
-    var grid = createGrid();
+    var grid = createGrid(gridModel);
     var sidebar = createSidebar();
 
     UIRoot.appendChild(grid);
