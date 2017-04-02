8b158787c2f5e9c2c6ed606fccdaae6e0a17cbfb
Connor Stack <sirconnorstack@gmail.com>
2017-04-01 18:01:23 -0700

    Visualize program counter and suppressed commands

--- js/components/command_list.tsx
+++ js/components/command_list.tsx
@@ -14,12 +14,19 @@ class CommandList extends React.Component<CommandListProps, CommandListState> {
     super(props);
   }
   render() {
-    let listItems = this.props.program.commands.map((command, i) =>
-      <li key={`command-list-${i}`}>
+    let listItems = this.props.program.commands.map((command, i) => {
+      let classes: Array<String> = [];
+      if (this.props.cell.activeMap[i] === false) {
+        classes.push("command-suppressed");
+      }
+      if (this.props.cell.programCounter === i) {
+        classes.push("command-current");
+      }
+      return <li key={`command-list-${i}`} className={classes.join(" ")}>
         <Command command={command} />
       </li>
-    );
-    return <ul>{listItems}</ul>;
+    });
+    return <ul className="command-list">{listItems}</ul>;
   }
 }
 
